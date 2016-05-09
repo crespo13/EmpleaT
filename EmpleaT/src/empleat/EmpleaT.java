daf7a791877cced1442020f2a4f4a9f664e74873
admin <admin@10.10.0.72>
2016-05-09 10:26:55 +0300

    He añadido un comentario

--- EmpleaT/src/empleat/EmpleaT.java
+++ EmpleaT/src/empleat/EmpleaT.java
@@ -5,6 +5,7 @@ public class EmpleaT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Word!");
+		// Todavia no he hecho commit de este comentario
 
 	}
 
